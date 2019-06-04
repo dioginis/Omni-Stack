69b69371d659293621f496de217cd95f1244055f
Dioginis <dioginiscarvalho@hotmail.com>
2019-06-03 23:38:17 -0300

    feat: finish frontend

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -8,7 +8,7 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Main} />
-      <Route path="/boxes/:id" component={Box} />
+      <Route path="/box/:id" component={Box} />
     </Switch>
   </BrowserRouter>
 );
